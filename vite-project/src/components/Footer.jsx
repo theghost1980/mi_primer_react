de0679d456f670a0d4b1283c0024a0b3b613ab1c
Kayntless <andresizqcast2000@gmail.com>
2026-04-13 16:14:39 -0400

    created footer

---
+++ vite-project/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <div>
+            <h1>Footer</h1>
+            <p>Me ahogue con cafe</p> 
+        </div>
+                <div>
+            <h1>Footer</h1>
+            <p>Me ahogue con cafe</p> 
+        </div>    
+    </div>
+  )
+}
