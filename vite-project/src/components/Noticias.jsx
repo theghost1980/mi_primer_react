a2eab787263829cc46a493defac6e500109724a4
theghost1980 <saturnob612@gmail.com>
2026-04-13 16:06:47 -0400

    updates in app

---
+++ vite-project/src/components/Noticias.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Noticias() {
+  return (
+    <div>
+      <h3>Noticias de ultima hora</h3>
+      <p>Se revisaron las lineas 1 y 2 de la red electrica</p>
+    </div>
+  );
+}
+
+export default Noticias;
